264b7052a2af998730e7fd9b19992c203effdd57
Samim Junayet Istiaq <junayet03@gmail.com>
2025-10-04 18:53:35 +0600

    add

---
+++ code/Flutter One/lib/service/authservice.dart
@@ -0,0 +1,19 @@
+
+
+
+class AuthService{
+
+  final String baseUrl = "http://localhost:8085";
+
+  // Future<bool> login(String email, String password) async{
+  //   final url = Uri
+  //
+  //
+  // }
+
+
+
+
+
+
+}
